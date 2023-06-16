93e94e49b51421e34a50ecec0003e9202f714408
PaiperleckMaroc <dev.maroc@paiperleck.lu>
2023-06-16 15:45:45 +0100

    change settings

--- QHSEBackend/settings.py
+++ QHSEBackend/settings.py
@@ -28,11 +28,15 @@ SECRET_KEY = 'django-insecure-)jh2%5o516bk=dlww130@9px&+koza@ejhb61(g#7y4oaxn=ob
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://api.paiperleckelearning.com','api.paiperleckelearning.com','http://api.paiperleckelearning.com']
+MIXED_CONTENT_INSECURE = True
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+CORS_ALLOWED_ORIGINS = ['https://paiperleckelearning.com',
+                        'https://dash.paiperleckelearning.com']
+
+SESSION_COOKIE_SECURE = True
 
 
-# Application definition
-CORS_ORIGIN_ALLOW_ALL = True
 #Email Settings 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
@@ -88,14 +92,6 @@ JWT_AUTH.update({
 })
 
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:4200",
-    "http://127.0.0.1:8000"
-]
-CORS_ALLOWED_HEADERS = [
-    'cache-control',  # Add 'cache-control' to the allowed headers
-    # Other allowed headers
-]
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
@@ -139,8 +135,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'qhsedb',
-        'USER': 'root',
-        'PASSWORD': '',
+        'USER': 'ATExperts',
+        'PASSWORD': 'ATExperts2023',
         'HOST': 'localhost',
         'PORT': '3306',
    
@@ -183,6 +179,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
