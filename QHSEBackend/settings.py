b4db58ffdde2087060b96b58810a22e3e425e9fb
HatimAbarki <dev6@paiperleck.lu>
2023-07-25 14:31:16 +0100

    update hatim v3.7.2

--- QHSEBackend/settings.py
+++ QHSEBackend/settings.py
@@ -28,12 +28,17 @@ SECRET_KEY = 'django-insecure-)jh2%5o516bk=dlww130@9px&+koza@ejhb61(g#7y4oaxn=ob
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://qhseapi.paiperleckelearning.com','qhseapi.paiperleckelearning.com','http://qhseapi.paiperleckelearning.com']
+ALLOWED_HOSTS = ['https://api.paiperleckelearning.com','api.paiperleckelearning.com','http://api.paiperleckelearning.com','127.0.0.1', 'localhost']
 MIXED_CONTENT_INSECURE = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-CORS_ALLOWED_ORIGINS = ['https://qhse.paiperleckelearning.com']
+CORS_ALLOWED_ORIGINS = ['https://paiperleckelearning.com',
+                        'https://dash.paiperleckelearning.com']
+
 SESSION_COOKIE_SECURE = True
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
+]
 #Email Settings 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
@@ -89,6 +94,10 @@ JWT_AUTH.update({
 })
 
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
+    "http://127.0.0.1:8000"
+]
 CORS_ALLOWED_HEADERS = [
     'cache-control',  # Add 'cache-control' to the allowed headers
     # Other allowed headers
@@ -136,8 +145,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'qhsedb',
-        'USER': 'ATExperts',
-        'PASSWORD': 'ATExperts2023',
+        'USER': 'root',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
    
